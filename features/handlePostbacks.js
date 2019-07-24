ca182209126112d1991cd05094c65f276baa11b7
Pavlo Huk <pavlo2001lviv@gmail.com>
2019-07-24 18:40:52 +0300

    added main menu postback function

--- features/handlePostbacks.js
+++ features/handlePostbacks.js
@@ -1,9 +1,12 @@
+const mainMenu = require('./postbacks/mainMenu');
+
 module.exports = (controller) => {
   controller.on('facebook_postback', (bot, message) => {
     const { payload } = message.postback;
 
     switch (payload) {
       case 'MAIN_MENU':
+        mainMenu(message);
         break;
       default:
         break;
