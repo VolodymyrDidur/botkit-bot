a8e7778e6566d6e24e6f32fa427300130d6beb80
Pavlo Huk <pavlo2001lviv@gmail.com>
2019-07-25 14:30:09 +0300

    added connection to mongo

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 const { Botkit } = require('botkit');
 const { FacebookAdapter, FacebookEventTypeMiddleware } = require('botbuilder-adapter-facebook');
+const { MongoDbStorage } = require('botbuilder-storage-mongodb');
 require('dotenv').config();
+
+const mongoStorage = new MongoDbStorage({
+  url: process.env.mongo_uri,
+});
 
 const facebookAdapter = new FacebookAdapter({
   access_token: process.env.access_token,
@@ -8,8 +13,10 @@ const facebookAdapter = new FacebookAdapter({
   app_secret: process.env.app_secret,
 });
 facebookAdapter.use(new FacebookEventTypeMiddleware());
+
 const controller = new Botkit({
   adapter: facebookAdapter,
+  storage: mongoStorage,
   webhook_uri: '/facebook/receive',
 });
 
