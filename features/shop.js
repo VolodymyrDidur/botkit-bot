f447909865a063f1a25603a7f63eb75be9161393
Pavlo Huk <pavlo2001lviv@gmail.com>
2019-07-28 16:33:49 +0300

    added choise of categories without working prev/next buttons

--- features/shop.js
+++ features/shop.js
@@ -1,7 +1,11 @@
+const categoriesTemplate = require('../bestbuy/categories');
+
 module.exports = (controller) => {
   controller.on('message', async (bot, message) => {
-    if (message.quick_reply.payload === 'SHOP') {
-      await bot.reply(message, 'shop');
-    }
+    await categoriesTemplate(1).then(async (categories) => {
+      if (message.quick_reply.payload === 'SHOP') {
+        await bot.reply(message, categories);
+      }
+    });
   });
 };
