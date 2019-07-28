7c620702e2b9b0e381b6c066350d838935a2ad66
Pavlo Huk <pavlo2001lviv@gmail.com>
2019-07-28 18:41:22 +0300

    added working prev/next buttons

--- features/shop.js
+++ features/shop.js
@@ -1,9 +1,20 @@
-const categoriesTemplate = require('../bestbuy/categories');
+const sendCategoriesPage = require('../bestbuy/categories');
 
 module.exports = (controller) => {
   controller.on('message', async (bot, message) => {
     if (message.quick_reply.payload === 'SHOP') {
-      await categoriesTemplate(1).then(async (categories) => {
+      global.categoriesPage = 1;
+      await sendCategoriesPage(global.categoriesPage).then(async (categories) => {
+        await bot.reply(message, categories);
+      });
+    } else if (message.quick_reply.payload === 'NEXT_CATEGORY_PAGE') {
+      global.categoriesPage += 1;
+      await sendCategoriesPage(global.categoriesPage).then(async (categories) => {
+        await bot.reply(message, categories);
+      });
+    } else if (message.quick_reply.payload === 'PREV_CATEGORY_PAGE') {
+      global.categoriesPage -= 1;
+      await sendCategoriesPage(global.categoriesPage).then(async (categories) => {
         await bot.reply(message, categories);
       });
     }
